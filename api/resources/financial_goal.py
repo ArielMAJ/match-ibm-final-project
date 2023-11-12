fb1d38c0be44d4dad105815f038c48b732b6bbf6
ArielMAJ <ariel.maj@hotmail.com>
2023-11-12 17:52:12 -0300

    [UPT] Finished front-back integration.

--- api/resources/financial_goal.py
+++ api/resources/financial_goal.py
@@ -5,7 +5,7 @@ Define the REST verbs relative to the FinancialGoal route.
 import math
 from http import HTTPStatus
 
-from flask import jsonify, make_response
+from flask import Request, Response, jsonify, make_response, render_template
 from flask_restful import Resource, request
 
 
@@ -15,27 +15,43 @@ class FinancialGoalResource(Resource):
     @staticmethod
     def post():
         """Returns the amount of months to reach the financial goal"""
-        if request.json is None:
+        response = FinancialGoalResource.get_response(request)
+        if request.is_json:
+            return response
+
+        return make_response(
+            render_template("index.html", message=response.json.pop("message")),
+            HTTPStatus.OK,
+            {"Content-Type": "text/html; charset=utf-8"},
+        )
+
+    @staticmethod
+    def get_response(request: Request) -> Response:
+        """Returns the response for the POST request"""
+        data = request.json if request.is_json else request.form
+        if not data:
             return make_response(
                 jsonify(
                     {
                         "months": None,
-                        "message": "Request should include a JSON in the body.",
+                        "message": "É esperado submissão de formulário ou um JSON.",
                     }
                 ),
                 HTTPStatus.BAD_REQUEST,
             )
-        if not (monthly_savings := request.json.pop("monthly_savings", None)):
+
+        if not (goal := data.get("goal")):
+            return make_response(
+                {"months": None, "message": 'Incluir "Meta Financeira".'},
+                HTTPStatus.BAD_REQUEST,
+            )
+
+        if not (monthly_savings := data.get("monthly_savings", None)):
             return make_response(
                 {
                     "months": None,
-                    "message": "Request should include the monthly savings.",
+                    "message": 'Incluir "Economia Mensal".',
                 },
-                HTTPStatus.BAD_REQUEST,
-            )
-        if not (goal := request.json.pop("goal")):
-            return make_response(
-                {"months": None, "message": "Request should include the goal."},
                 HTTPStatus.BAD_REQUEST,
             )
         try:
@@ -45,26 +61,41 @@ class FinancialGoalResource(Resource):
             return make_response(
                 {
                     "months": None,
-                    "message": "Monthly savings and goal should be numbers.",
+                    "message": "Economia Mensal e Meta Financeira devem ser números.",
                 },
                 HTTPStatus.BAD_REQUEST,
             )
         if monthly_savings <= 0:
             return make_response(
-                {"months": None, "message": "Monthly savings should be positive."},
+                {
+                    "months": None,
+                    "message": "Economia Mensal deve ser um valor numérico positivo.",
+                },
                 HTTPStatus.BAD_REQUEST,
             )
         if goal <= 0:
             return make_response(
-                {"months": None, "message": "Goal should be positive."},
+                {
+                    "months": None,
+                    "message": '"Meta Financeira" deve ser um valor numérico positivo.',
+                },
                 HTTPStatus.BAD_REQUEST,
             )
 
-        months: int = math.ceil(goal / monthly_savings)
+        months: int = FinancialGoalResource.caculate_months(goal, monthly_savings)
+
         return make_response(
-            {
-                "months": months,
-                "message": f"It will take {months} months to reach your goal.",
-            },
+            jsonify(
+                {
+                    "months": months,
+                    "message": (
+                        f"Levará {months} meses para atingir sua Meta Financeira."
+                    ),
+                }
+            ),
             HTTPStatus.OK,
         )
+
+    @staticmethod
+    def caculate_months(goal: int | float, monthly_savings: int | float) -> int:
+        return math.ceil(goal / monthly_savings)
